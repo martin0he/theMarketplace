6deae5c6db7e020a4b3dc69f99f4b54af6240e96
martin0he <martin.hema.mh@gmail.com>
2024-05-21 15:51:12 +0200

    preview coming along

--- frontend/src/components/PreviewTab.tsx
+++ frontend/src/components/PreviewTab.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import { useAuth } from "../auth/AuthProvider";
 import useWindowDimensions from "../hooks/useWindowDimensions";
 
@@ -29,115 +29,110 @@ const PreviewTab = ({
         padding: "10px",
       }}
     >
-      <Typography
-        fontFamily="Josefin Sans"
-        sx={{
-          fontSize: "29px",
-          marginBottom: "10px",
-          fontWeight: "bold",
-          marginLeft: "16px",
-        }}
-      >
-        {listingName}
-      </Typography>
-      <Typography
-        fontFamily="Josefin Sans"
-        sx={{ margin: "5px 0", fontSize: "19px" }}
-      >
-        <strong>Posted: </strong>
-        {formattedDate}
-      </Typography>
-      <Typography
-        fontFamily="Josefin Sans"
-        sx={{ margin: "5px 0", fontSize: "19px" }}
-      >
-        <strong>By: </strong>
-        {customUser?.username}
-      </Typography>
-      <Typography
-        fontFamily="Josefin Sans"
-        sx={{ margin: "5px 0", fontSize: "19px" }}
-      >
-        <strong>Condition: </strong>
-        {listingCondition}
-      </Typography>
-      <Box
-        width="wrap-content"
-        maxWidth={0.35 * width}
-        height={0.25 * height}
-        sx={{
-          overflow: "hidden",
-        }}
-      >
-        <Typography
-          fontFamily="Josefin Sans"
-          sx={{
-            margin: "5px 0",
-            fontSize: "19px",
-            wordWrap: "break-word",
-            whiteSpace: "pre-wrap",
-          }}
-        >
-          <strong>Description: </strong>
-          {listingDescription}
-        </Typography>
-      </Box>
-
-      <Box sx={{ marginTop: "20px" }}>
-        <Typography
-          fontFamily="Josefin Sans"
-          sx={{ margin: "5px 0", fontSize: "19px" }}
-        >
-          <strong>Payment: </strong>
-        </Typography>
-        <ul style={{ paddingLeft: "18px", margin: 0 }}>
-          {listingPaymentMethods.map((method, index) => (
-            <li key={index} style={{ fontSize: "18px" }}>
-              {method}
-            </li>
-          ))}
-        </ul>
-      </Box>
-      <Box sx={{ marginTop: "20px" }}>
-        <Typography
-          variant="body2"
-          sx={{ fontSize: "16px", fontWeight: "bold" }}
-        >
-          Exchange:
-        </Typography>
-        <Typography variant="body2" sx={{ fontSize: "16px" }}>
-          Location: {listingLocation}
-        </Typography>
-      </Box>
-      <Box
-        sx={{
-          position: "relative",
-          width: "300px",
-          height: "200px",
-          marginTop: "20px",
-          border: "1px solid #ced4da",
-          borderRadius: "8px",
-          backgroundColor: "rgba(0, 0, 255, 0.2)",
-        }}
-      >
-        <Box
-          sx={{
-            position: "absolute",
-            bottom: "10px",
-            right: "10px",
-            backgroundColor: "#d0e8e0",
-            padding: "5px 10px",
-            borderRadius: "8px",
-          }}
-        >
+      <Grid container>
+        <Grid item>
           <Typography
-            variant="body2"
-            sx={{ fontSize: "16px", fontWeight: "bold" }}
+            fontFamily="Josefin Sans"
+            sx={{
+              fontSize: "33px",
+              marginBottom: "10px",
+              fontWeight: "bold",
+            }}
           >
-            ${listingPrice}
+            {listingName ? `${listingName} for sale` : "Name not specified"}
           </Typography>
-        </Box>
-      </Box>
+        </Grid>
+
+        <Grid item container>
+          <Grid item xs={12} md={6}>
+            <Box>
+              <Typography
+                fontFamily="Josefin Sans"
+                sx={{ margin: "5px 0", fontSize: "19px" }}
+              >
+                <strong>Posted: </strong>
+                {formattedDate}
+              </Typography>
+              <Typography
+                fontFamily="Josefin Sans"
+                sx={{ margin: "5px 0", fontSize: "19px" }}
+              >
+                <strong>By: </strong>
+                {customUser?.username}
+              </Typography>
+              <Typography
+                fontFamily="Josefin Sans"
+                sx={{ margin: "5px 0", fontSize: "19px" }}
+              >
+                <strong>Condition: </strong>
+                {listingCondition ? `${listingCondition}` : "Not Selected"}
+              </Typography>
+              <Box
+                sx={{
+                  display: "inline-block",
+                  maxWidth: 0.35 * width,
+                  maxHeight: 0.25 * height,
+                  overflow: "hidden",
+                  width: "fit-content",
+                  height: "fit-content",
+                  wordWrap: "break-word",
+                  whiteSpace: "pre-wrap",
+                }}
+              >
+                <Typography
+                  fontFamily="Josefin Sans"
+                  sx={{
+                    margin: "5px 0",
+                    fontSize: "19px",
+                    wordWrap: "break-word",
+                    whiteSpace: "pre-wrap",
+                  }}
+                >
+                  <strong>Description: </strong>
+                  {listingDescription ? `${listingDescription}` : "N/A"}
+                </Typography>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Box
+              width="300px"
+              height="300px"
+              marginBottom="25px"
+              sx={{ backgroundColor: "green" }}
+            ></Box>
+          </Grid>
+        </Grid>
+
+        <Grid container item>
+          <Grid item xs={12} md={6}>
+            <Typography
+              fontFamily="Josefin Sans"
+              sx={{ margin: "5px 0", fontSize: "19px" }}
+            >
+              <strong>Payment: </strong>
+            </Typography>
+            <ul style={{ paddingLeft: "18px", margin: "3px" }}>
+              {listingPaymentMethods && listingPaymentMethods.length > 0
+                ? listingPaymentMethods.map((method, index) => (
+                    <li key={index} style={{ fontSize: "18px" }}>
+                      {method}
+                    </li>
+                  ))
+                : "N/A"}
+            </ul>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Typography
+              fontFamily="Josefin Sans"
+              sx={{ margin: "5px 0", fontSize: "19px" }}
+            >
+              <strong>Exchange At: </strong>
+              {listingLocation ? `${listingLocation}` : "Not Specified"}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
