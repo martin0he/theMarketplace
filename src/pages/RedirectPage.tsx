0705f1708993b3a7ebeb202163ee981115d06eeb
martin0he <martin.hema.mh@gmail.com>
2024-06-27 17:28:05 +0200

    lower margins on redirect

--- src/pages/RedirectPage.tsx
+++ src/pages/RedirectPage.tsx
@@ -2,10 +2,10 @@ import { Box, Typography } from "@mui/material";
 
 const RedirectPage = () => {
   return (
-    <Box paddingX="20px" marginTop="100px">
+    <Box paddingX="20px" marginTop="40px">
       <Typography>
         theMarketplace is intended to be used on a computer, so for the most
-        enjoyable experience get off your phone and open your laptop :) thank
+        enjoyable experience, get off your phone and open your laptop :) thank
         you.
       </Typography>
     </Box>
